30c75e9bb29b7955f4e040ac5d88fd41db229002
eiko03 <tipusultan50@gmail.com>
2021-10-08 12:49:24 +0600

    login form design

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,84 @@
 <template>
-  <div>
+  <div class="w-full max-w-x">
     <h4>Login</h4>
-    <form>
-      <label for="email">E-Mail Address</label>
-      <div>
-        <input id="email" type="email" v-model="email" required autofocus />
+    <form
+      class="
+        flex flex-col
+        justify-center
+        items-center
+        bg-white
+        shadow-md
+        rounded
+        px-8
+        pt-6
+        pb-8
+        mb-4
+      "
+    >
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="email"
+        >E-Mail Address</label
+      >
+      <div class="mb-6">
+        <input
+          id="email"
+          type="email"
+          v-model="email"
+          required
+          autofocus
+          class="
+            shadow
+            appearance-none
+            border
+            rounded
+            w-full
+            py-2
+            px-3
+            text-gray-700
+            leading-tight
+            focus:outline-none focus:shadow-outline
+          "
+        />
       </div>
-      <div>
-        <label for="password">Password</label>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password"
+          >Password</label
+        >
         <div>
-          <input id="password" type="password" v-model="password" required />
+          <input
+            class="
+              shadow
+              appearance-none
+              border border-red-500
+              rounded
+              w-full
+              py-2
+              px-3
+              text-gray-700
+              mb-3
+              leading-tight
+              focus:outline-none focus:shadow-outline
+            "
+            id="password"
+            type="password"
+            v-model="password"
+            required
+          />
         </div>
       </div>
       <div>
-        <button type="submit" @click="handleSubmit">Login</button>
+        <button
+          class="
+            inline-block
+            align-baseline
+            font-bold
+            text-sm text-blue-500
+            hover:text-blue-800
+          "
+          type="submit"
+          @click="handleSubmit"
+        >
+          Login
+        </button>
       </div>
     </form>
   </div>
