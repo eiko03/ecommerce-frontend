4ee27fd11b017bd8b9aa87be3a4fff7927fa1239
eiko03 <tipusultan50@gmail.com>
2021-10-08 10:56:34 +0600

    added env support

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 let app;
 dotenv.config();
 const axiosInstance = Axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.BASE_API_URL,
 });
 
 app = createApp(App).use(store).use(router).mount("#app");
