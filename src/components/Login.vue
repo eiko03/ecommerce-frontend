0ff1c5d09e0054d5cb5d963d35d99ae65514f75b
eiko03 <tipusultan50@gmail.com>
2021-10-08 15:34:09 +0600

    router

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -85,6 +85,7 @@
 </template>
 
 <script>
+import api from "@/service/api.js";
 export default {
   data() {
     return {
@@ -96,32 +97,44 @@ export default {
     handleSubmit(e) {
       e.preventDefault();
       if (this.password.length > 0) {
-        this.$http
-          .post("/auth/login", {
-            email: this.email,
-            password: this.password,
-          })
+        let paylaod = {
+          email: this.email,
+          password: this.password,
+        };
+        api
+          .login(paylaod)
           .then((response) => {
-            let is_admin = response.data.user.is_admin;
-            localStorage.setItem("user", JSON.stringify(response.data.user));
-            localStorage.setItem("jwt", response.data.token);
-
-            if (localStorage.getItem("jwt") != null) {
-              this.$emit("loggedIn");
-              if (this.$route.params.nextUrl != null) {
-                this.$router.push(this.$route.params.nextUrl);
-              } else {
-                if (is_admin == 1) {
-                  this.$router.push("admin");
-                } else {
-                  this.$router.push("dashboard");
-                }
-              }
-            }
+            console.log(response);
           })
-          .catch(function (error) {
-            console.error(error.response);
+          .catch((err) => {
+            console.log(err);
           });
+        // this.$http
+        //   .post("api/auth/login", {
+        //     email: this.email,
+        //     password: this.password,
+        //   })
+        //   .then((response) => {
+        //     let is_admin = response.data.user.is_admin;
+        //     localStorage.setItem("user", JSON.stringify(response.data.user));
+        //     localStorage.setItem("jwt", response.data.token);
+        //
+        //     if (localStorage.getItem("jwt") != null) {
+        //       this.$emit("loggedIn");
+        //       if (this.$route.params.nextUrl != null) {
+        //         this.$router.push(this.$route.params.nextUrl);
+        //       } else {
+        //         if (is_admin == 1) {
+        //           this.$router.push("admin");
+        //         } else {
+        //           this.$router.push("dashboard");
+        //         }
+        //       }
+        //     }
+        //   })
+        //   .catch(function (error) {
+        //     console.error(error.response);
+        //   });
       }
     },
   },
