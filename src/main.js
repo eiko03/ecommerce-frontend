fd3937f23aada1a84ce2b6f99c78d1fa73f32cda
eiko03 <tipusultan50@gmail.com>
2021-10-08 10:33:40 +0600

    authentication

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
+import Axios from "axios";
 
-createApp(App).use(store).use(router).mount("#app");
+
+const app= createApp(App).use(store).use(router).mount("#app");
+app.provide("$axios", Axios);
